383b1a827d8a2955ddcc6bd472135a35629b21a9
flacy <me@flacy.me>
2020-12-28 17:03:12 +0000

    updated version

--- base_cfg/base_cfg.go
+++ base_cfg/base_cfg.go
@@ -1,7 +1,7 @@
 package base_cfg
 
 const (
-	VERSION = "0.0.2"
+	VERSION = "0.0.2a"
 	CONFIG_NAME = "upip"
 	BASE_PACKAGE_URL = "https://pypi.org/simple/"
 	MAX_MESSAGE_LENGTH = 30 // max letters in progress bar
