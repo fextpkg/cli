387a9316de09a49a37a930bde1408832068dabf4
flacy <me@flacy.me>
2020-12-29 14:17:03 +0000

    renamed config name

--- base_cfg/base_cfg.go
+++ base_cfg/base_cfg.go
@@ -2,7 +2,7 @@ package base_cfg
 
 const (
 	VERSION = "0.0.2b"
-	CONFIG_NAME = "upip"
+	CONFIG_NAME = "fext"
 	BASE_PACKAGE_URL = "https://pypi.org/simple/"
 	MAX_MESSAGE_LENGTH = 30 // max letters in progress bar
 	DEFAULT_CHMOD = 0775
