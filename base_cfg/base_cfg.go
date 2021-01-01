50996c166ba86478700286663a15fb3d3711ca63
flacy <me@flacy.me>
2021-01-01 10:26:22 +0000

    updated version

--- base_cfg/base_cfg.go
+++ base_cfg/base_cfg.go
@@ -1,7 +1,7 @@
 package base_cfg
 
 const (
-	VERSION = "0.0.2b"
+	VERSION = "0.0.3"
 	CONFIG_NAME = "fext"
 	BASE_PACKAGE_URL = "https://pypi.org/simple/"
 	MAX_MESSAGE_LENGTH = 30 // max letters in progress bar
