64161c4acbab477579a489070c749d83fb794be4
Lunte <offer@flacy.me>
2023-03-01 02:12:33 +0300

    Add docstrings

--- fext/utils/versions.go
+++ fext/utils/versions.go
@@ -7,6 +7,9 @@ import (
 	"unicode"
 )
 
+// parseVersion splits the version into semantic parts (major, minor, patch, pre)
+// and returns them in the form [3]int{major, minor, patch}, pre. Returns an error
+// if version could not be converted to int
 func parseVersion(s string) ([3]int, int, error) {
 	var output [3]int
 	var pre int
@@ -32,6 +35,9 @@ func parseVersion(s string) ([3]int, int, error) {
 	return output, pre, nil
 }
 
+// parsePreVersion separates patch and pre version. Example: 1a2 => 1, 147
+// (a is 97, 2 is 50 => 97 + 50 = 147). Returns an error if version could not be
+// converted to int
 func parsePreVersion(s string) (int, int, error) {
 	var patchValue, preValue int
 	var err error
@@ -59,6 +65,8 @@ func convertStrToInt(s string) (int, error) {
 	return output, nil
 }
 
+// compareVersion returns the result of a comparison between versions. If a > b,
+// 1 will be returned, if a < b -1 will be returned, if a == b 0 will be returned
 func compareVersion(a, b string) (int, error) {
 	v1, v1pre, err := parseVersion(a)
 	if err != nil {
@@ -88,6 +96,16 @@ func compareVersion(a, b string) (int, error) {
 	return 0, nil
 }
 
+// CompareVersion works by means of comparing each part of the version. The
+// version is divided into semantic parts (major, minor, patch, pre) and
+// converted into numbers that are compared one after another. For example:
+// 4.0.0a >= 4.0.0rc2 will return the result false because alpha build has less
+// weight than release candidate build.
+//
+// The version is parsed according to the PEP 440 standard, which means that the
+// semantic version cannot be compared using it. Comparison of pre-release
+// versions occurs only if they are specified in both versions. Otherwise, the
+// check will be skipped
 func CompareVersion(v1, op, v2 string) (bool, error) {
 	res, err := compareVersion(v1, v2)
 	if err != nil {
@@ -109,11 +127,12 @@ func CompareVersion(v1, op, v2 string) (bool, error) {
 	return false, nil
 }
 
-// Split name, operator and version. Returns [][]string{operator, version} if operator was split successful
+// SplitOperators split name, operator and version. Returns name,
+// [][]string{operator, version}
 func SplitOperators(name string) (string, [][]string) {
 	var operators [][]string
 	// parse operators and split them. E.g. "name<=4.0.0 >=4.0.0" => [[<=, 4.0.0], [>=, 4.0.0]]
-	// NOTE: separator can be anything and it also may not exists
+	// NOTE: separator can be anything, and it also may not exist
 	re, _ := regexp.Compile(`([<>!=]=?)([\d\w\.]+)`)
 	v := re.FindAllStringSubmatch(strings.ReplaceAll(name, " ", ""), -1)
 
