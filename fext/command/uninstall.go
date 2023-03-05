45b765063618ee4eb029729d396aa6e50d7e9433
Lunte <offer@flacy.me>
2023-03-05 09:27:46 +0300

    Implement uninstall

--- fext/command/uninstall.go
+++ fext/command/uninstall.go
@@ -1,19 +1,49 @@
 package command
 
-func Uninstall(args []string) {
-	//options, offset := utils.ParseOptions(args)
-	//var collectDependency bool
-	//
-	//for _, option := range options {
-	//	switch option {
-	//	case "h", "help":
-	//		ui.PrintHelpUninstall()
-	//		return
-	//	case "w", "with-dependencies":
-	//		collectDependency = true
-	//	default:
-	//		ui.PrintUnknownOption(option, ui.PrintHelpUninstall)
-	//		return
-	//	}
-	//}
+import (
+	"github.com/fextpkg/cli/fext/config"
+	"github.com/fextpkg/cli/fext/pkg"
+	"github.com/fextpkg/cli/fext/ui"
+)
+
+var (
+	optCollectDep bool
+)
+
+func uninstall(pkgName string) error {
+	p, err := pkg.Load(pkgName)
+	if err != nil {
+		return err
+	}
+	if err = p.Uninstall(); err != nil {
+		return err
+	}
+	if optCollectDep {
+		for _, dep := range p.Dependencies {
+			uninstall(dep.Name)
+		}
+	}
+	return nil
+}
+
+func Uninstall(packages []string) {
+	for _, f := range config.Flags {
+		switch f {
+		case "h", "help":
+			ui.PrintHelpUninstall()
+			return
+		case "w", "with-dependencies":
+			optCollectDep = true
+		default:
+			ui.PrintUnknownOption(f, ui.PrintHelpUninstall)
+			return
+		}
+	}
+
+	for _, pkgName := range packages {
+		err := uninstall(pkgName)
+		if err != nil {
+			ui.PrintfError("Uninstall %s: %v\n", pkgName, err)
+		}
+	}
 }
